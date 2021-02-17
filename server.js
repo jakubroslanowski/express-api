04fd1f4d0354496fa6f9745429ffaf942b673e9b
Jakub Rosłanowski <jakub.roslanowski@gmail.com>
2021-02-17 15:44:32 +0100

    Prepare app for deployment

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 app.use(cors());
-app.use(express.static(path.join(__dirname + '/client/public')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 app.use((req, res) => {
   res.status(404).send('Error 404...');
 });
-app.listen(8000, () => {
-  console.log('Server connected');
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
